7e5e6995e8e0271f0b9b4400071316131de40b09
mohammad abed <mabed4297@gmail.com>
2019-05-18 12:39:13 +0300

    fixed gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-
 const Index = () => {
-  return <div>Hello Cinema Today !</div>;
+  return <div>Hello Cinema Today ! + {process.env.API_KEY} </div>;
 };
 
 export default Index;
