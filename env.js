1e4c8ac5b86f56b5ca4c5f679cdeb082dcbb768b
mohammad abed <mabed4297@gmail.com>
2019-05-18 17:56:45 +0300

    modified gitignore

--- env.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = "b52a731a9ee49b858715b3b7b84aeb2f";
-export const API_MOVIE = "https://api.themoviedb.org/3/movie";
-export const API_ACCESS_TOKEN =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNTJhNzMxYTllZTQ5Yjg1ODcxNWIzYjdiODRhZWIyZiIsInN1YiI6IjVjZGMyZjBlOTI1MTQxMmEzOGNlYzFmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yEk_Q5iAhl9arthM9VpsOkWTCOXYJ-lejpnRTZoddVs";
