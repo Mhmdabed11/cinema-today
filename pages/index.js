2bb264d692efdbef2d0d1714b5eedc30bc25c440
mohammad abed <mabed4297@gmail.com>
2019-05-19 15:54:06 +0300

    added custom carousel

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,10 @@
 import React from "react";
 import Layout from "../src/components/layout";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { getMovieById } from "../src/api/get-movies/get-movie-by-id";
 import { getMoviesPlayingNow } from "../src/api/get-movies/get-movies-playing-now";
 import { getApiConfig } from "../src/api/config/get-api-config";
+import Carousel from "../src/components/carousel";
 const Index = props => {
-  const sliderRef = React.useRef();
-
-  var settings = {
-    infinite: false,
-    speed: 500,
-    slidesToShow: 8,
-    slidesToScroll: 8,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1300,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 6
-        }
-      },
-      {
-        breakpoint: 1130,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 5,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 940,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4
-        }
-      },
-      {
-        breakpoint: 760,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-      {
-        breakpoint: 570,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 375,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
   const { movies, base_url, poster_size } = props;
   const previous = () => {
     sliderRef.current.slickPrev();
@@ -71,15 +15,11 @@ const Index = props => {
   return (
     <Layout>
       <div>
-        <Slider ref={sliderRef} {...settings}>
-          {Object.keys(movies).map(movie => (
-            <div style={{ margin: "0 auto" }}>
-              <img
-                src={`${base_url}${poster_size}${movies[movie].poster_path}`}
-              />
-            </div>
-          ))}
-        </Slider>
+        <Carousel
+          movies={movies}
+          base_url={base_url}
+          poster_size={poster_size}
+        />
         <pre>{/* <code>{JSON.stringify(props, null, 2)}</code> */}</pre>
       </div>
     </Layout>
---
+++ src/components/carousel/index.js
@@ -0,0 +1,93 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./carousel.css";
+
+function Carousel({ movies, base_url, poster_size }) {
+  const [x, setX] = React.useState(0);
+  const [width, setWidth] = React.useState(0);
+  const [totalTrans, setTotlaTrans] = React.useState(0);
+  const [activeItems, setActiveItems] = React.useState(0);
+  const setInnerWidth = () => {
+    setWidth(window.innerWidth);
+    setTotlaTrans(0);
+    setX(0);
+    const { innerWidth } = window;
+    setWidth(innerWidth);
+    if (innerWidth >= 1000) {
+      setActiveItems(6);
+    } else if (innerWidth < 1000 && innerWidth >= 700) {
+      setActiveItems(5);
+    } else if (innerWidth < 700 && innerWidth >= 500) {
+      setActiveItems(4);
+    }
+  };
+
+  React.useEffect(() => {
+    if (window) {
+      const { innerWidth } = window;
+      setWidth(innerWidth);
+      if (innerWidth >= 1000) {
+        setActiveItems(6);
+      } else if (innerWidth < 1000 && innerWidth >= 700) {
+        setActiveItems(5);
+      } else if (innerWidth < 700 && innerWidth >= 500) {
+        setActiveItems(4);
+      }
+    }
+  }, []);
+  React.useEffect(() => {
+    window.addEventListener("resize", setInnerWidth);
+    return () => {
+      window.removeEventListener("resize", setInnerWidth);
+    };
+  });
+
+  const next = () => {
+    setX(x => x + 1);
+    setTotlaTrans(totalTrans => totalTrans - 100 / activeItems);
+  };
+  const prev = () => {
+    setX(x => x - 1);
+    setTotlaTrans(totalTrans => totalTrans + 100 / activeItems);
+  };
+  return (
+    <>
+      <div>
+        {x}, {width}px, {activeItems}
+      </div>
+      <div className="slider">
+        <div className="showPeek sliderMask">
+          <div
+            style={{
+              transform: `translateX(${totalTrans}%)`
+            }}
+            className="sliderContent row-with-x-columns"
+            onTouchStart={e => console.log(e)}
+          >
+            {Object.keys(movies).map(movie => (
+              <div className="slider-item">
+                <img
+                  src={`${base_url}${poster_size}${movies[movie].poster_path}`}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <button disabled={x === 0} onClick={prev}>
+        prev
+      </button>
+      <button
+        className="next"
+        disabled={x === movies.length - activeItems}
+        onClick={next}
+      >
+        next
+      </button>
+    </>
+  );
+}
+
+export default Carousel;
