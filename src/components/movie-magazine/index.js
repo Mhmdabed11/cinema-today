91bc31c3fa85ad87888d41e12d57d81b141c46e3
Mohammad <mohd.abed@saugo360.com>
2019-05-22 10:18:42 +0300

    added custom routes for movie page in now.json

--- src/components/movie-magazine/index.js
+++ src/components/movie-magazine/index.js
@@ -11,19 +11,19 @@ const MovieMagazine = ({ movies, base_url, poster_size, title }) => {
 
       <Carousel movies={movies} base_url={base_url} poster_size={poster_size}>
         {Object.keys(movies).map((movie, _index) => (
-          // <Link
-          //   key={_index}
-          //   as={`/movie/${movies[movie].id}`}
-          //   href={`/movie?id=${movies[movie].id}`}
-          // >
-          //   <a>
-          <CarouselItem>
-            <img
-              src={`${base_url}${poster_size}${movies[movie].poster_path}`}
-            />
-          </CarouselItem>
-          /* </a>
-          </Link> */
+          <Link
+            key={_index}
+            as={`/movie/${movies[movie].id}`}
+            href={`/movie?id=${movies[movie].id}`}
+          >
+            <a>
+              <CarouselItem>
+                <img
+                  src={`${base_url}${poster_size}${movies[movie].poster_path}`}
+                />
+              </CarouselItem>
+            </a>
+          </Link>
         ))}
       </Carousel>
     </div>
