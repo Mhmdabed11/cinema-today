543200638961f30d90960440a2dc049273cec616
mohammad abed <mabed4297@gmail.com>
2019-06-08 14:55:48 +0300

    minor bugfix

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -25,6 +25,7 @@ const Layout = ({ children }) => {
         html {
           margin: 0;
           padding: 0;
+          overflow-x: hidden;
         }
         * {
           box-sizing: border-box;
